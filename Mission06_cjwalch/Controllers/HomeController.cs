2e160e78e7957bda8a6aa53afe09081446673882
CarterSos <carterjwalch@gmail.com>
2023-02-13 21:14:25 -0700

    Add project files.

---
+++ Mission06_cjwalch/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using DateMe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DateApplicationContext blahContext { get; set; }
+        // Constructor
+        public HomeController(ILogger<HomeController> logger, DateApplicationContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        //we added this
+        [HttpGet]
+        public IActionResult DatingApplication() // GET
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DatingApplication(ApplicationResponse ar) // POST
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges(); 
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_cjwalch/Models/DateApplicationContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMe.Models
+{
+    public class DateApplicationContext : DbContext
+    {
+        // constructor
+        public DateApplicationContext(DbContextOptions<DateApplicationContext> options) : base(options)
+        {
+            // leave blank for now
+        }
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    Title = "Avengers: Endgame",
+                    Category = "Action",
+                    Year = 2019,
+                    Director = "Russo Brothers",
+                    Rating = "PG-13",
+                    Notes = "Greatest Movie ever"
+                },
+                new ApplicationResponse
+                {
+                    Title = "Spiderman: Into the Spider-verse",
+                    Category = "Action",
+                    Year = 2018,
+                    Director = "Bob Persichetti",
+                    Rating = "PG"
+                },
+                new ApplicationResponse
+                {
+                    Title = "The Count of Monte Cristo",
+                    Category = "Adventure",
+                    Year = 2002,
+                    Director = "Kevin Reynolds",
+                    Rating = "PG-13",
+                    Notes = "Best revenge story"
+                }
+
+            );
+        }
+    }
+}
